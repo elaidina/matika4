578e2f31ba9ef17850f95230b55b1562ceeacbbc
Your Name <you@example.com>
2022-10-25 13:48:51 +0200

    first

---
+++ script.js
@@ -0,0 +1,425 @@
+const quizData = [
+    {
+        question: "15:3 =",
+        a: "2",
+        b: "3",
+        c: "4",
+        d: "5",
+        correct: "d",
+    },
+    {
+        question: "49:7 =",
+        a: "7",
+        b: "6",
+        c: "12",
+        d: "9",
+        correct: "a",
+    },
+    {
+        question: "12:2 =",
+        a: "7",
+        b: "6",
+        c: "5",
+        d: "4",
+        correct: "b",
+    },
+    {
+        question: "7.8 =",
+        a: "64",
+        b: "24",
+        c: "56",
+        d: "69",
+        correct: "c",
+    },
+    {
+      question: "32-4=",
+      a: "20",
+      b: "24",
+      c: "25",
+      d: "28",
+      correct: "d",
+  },{
+    question: "15.4 =",
+    a: "30",
+    b: "40",
+    c: "45",
+    d: "60",
+    correct: "d",
+  },{
+    question: "(10.10)-100 =",
+    a: "0",
+    b: "10",
+    c: "1000",
+    d: "10 000",
+    correct: "a",
+  },{
+    question: "12+13 =",
+    a: "30",
+    b: "25",
+    c: "10",
+    d: "20",
+    correct: "b",
+  },{
+    question: "15-8 =",
+    a: "7",
+    b: "8",
+    c: "9",
+    d: "10",
+    correct: "a",
+  },{
+    question: "22+23 =",
+    a: "45",
+    b: "50",
+    c: "55",
+    d: "60",
+    correct: "a",
+  },{
+    question: "25-14 =",
+    a: "21",
+    b: "18",
+    c: "11",
+    d: "14",
+    correct: "c",
+  },{
+    question: "95+15=",
+    a: "100",
+    b: "105",
+    c: "110",
+    d: "120",
+    correct: "c",
+  },{
+    question: "150+50-50 +50 =",
+    a: "50",
+    b: "100",
+    c: "150",
+    d: "200",
+    correct: "d",
+  },{
+    question: "18:2 =",
+    a: "2",
+    b: "4",
+    c: "6",
+    d: "9",
+    correct: "d",
+  },{
+    question: "2.2.2.2 =",
+    a: "4",
+    b: "6",
+    c: "8",
+    d: "16",
+    correct: "d",
+  },{
+    question: "9+2+9=",
+    a: "15",
+    b: "20",
+    c: "25",
+    d: "0",
+    correct: "b",
+  },{
+    question: "10-14 =",
+    a: "-1",
+    b: "0",
+    c: "-3",
+    d: "10",
+    correct: "a",
+  },{
+    question: "50+12-2 =",
+    a: "60",
+    b: "62",
+    c: "64",
+    d: "52",
+    correct: "a",
+  },{
+    question: "100+150 =",
+    a: "100",
+    b: "150",
+    c: "200",
+    d: "250",
+    correct: "d",
+  },{
+    question: "15+16 =",
+    a: "30",
+    b: "31",
+    c: "32",
+    d: "33",
+    correct: "b",
+  },{
+    question: "27-9 =",
+    a: "16",
+    b: "17",
+    c: "18",
+    d: "19",
+    correct: "c",
+  },{
+    question: "6.3 =",
+    a: "18",
+    b: "19",
+    c: "20",
+    d: "21",
+    correct: "a",
+  },{
+    question: "66+44 =",
+    a: "110",
+    b: "93",
+    c: "96",
+    d: "99",
+    correct: "a",
+  },{
+    question: "16.2 =",
+    a: "26",
+    b: "18",
+    c: "30",
+    d: "32",
+    correct: "c",
+  },{
+    question: "13+9 =",
+    a: "18",
+    b: "22",
+    c: "24",
+    d: "27",
+    correct: "b",
+  },{
+    question: "20-4 =",
+    a: "12",
+    b: "14",
+    c: "16",
+    d: "18",
+    correct: "c",
+  },{
+    question: "5+6+6 =",
+    a: "10",
+    b: "15",
+    c: "17",
+    d: "20",
+    correct: "c",
+  },{
+    question: "37+12 =",
+    a: "39",
+    b: "49",
+    c: "50",
+    d: "60",
+    correct: "b",
+  },{
+    question: "29-11 =",
+    a: "16",
+    b: "17",
+    c: "18",
+    d: "19",
+    correct: "c",
+  },{
+    question: "6+8 =",
+    a: "11",
+    b: "12",
+    c: "13",
+    d: "14",
+    correct: "d",
+  },{
+    question: "6.5 =",
+    a: "18",
+    b: "20",
+    c: "22",
+    d: "30",
+    correct: "d",
+  },{
+    question: "8.3 =",
+    a: "24",
+    b: "28",
+    c: "32",
+    d: "36",
+    correct: "a",
+  },{
+    question: "55-23 =",
+    a: "22",
+    b: "32",
+    c: "42",
+    d: "17",
+    correct: "b",
+  },{
+    question: "9.3 =",
+    a: "14",
+    b: "21",
+    c: "27",
+    d: "35",
+    correct: "c",
+  },{
+    question: "4.4 =",
+    a: "8",
+    b: "12",
+    c: "16",
+    d: "20",
+    correct: "c",
+  },{
+    question: "9:2 =",
+    a: "2",
+    b: "2,5",
+    c: "3",
+    d: "4,5",
+    correct: "d",
+  },{
+    question: "50+60 =",
+    a: "140",
+    b: "150",
+    c: "160",
+    d: "110",
+    correct: "d",
+  },{
+    question: "99-19 =",
+    a: "90",
+    b: "99",
+    c: "89",
+    d: "80",
+    correct: "d",
+  },{
+    question: "(6.6)-6 =",
+    a: "12",
+    b: "24",
+    c: "30",
+    d: "48",
+    correct: "c",
+  },{
+    question: "12+98 =",
+    a: "99",
+    b: "100",
+    c: "101",
+    d: "110",
+    correct: "d",
+  },
+  ];
+  
+  const quiz = document.getElementById('quiz')
+  const answerEls = document.querySelectorAll('.answer')
+  const questionEl = document.getElementById('question')
+  const result = document.getElementById('resultquestion')
+  const a_text = document.getElementById('a_text')
+  const b_text = document.getElementById('b_text')
+  const c_text = document.getElementById('c_text')
+  const d_text = document.getElementById('d_text')
+  const submitBtn = document.getElementById('submit')
+  
+  let currentQuiz = 0
+  let score = 0
+  
+  loadQuiz()
+  
+  function loadQuiz() {
+    deselectAnswers()
+  
+    const currentQuizData = quizData[currentQuiz]
+  
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+  }
+  
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+  }
+  
+  function getSelected() {
+    let answer
+  
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+  
+    return answer
+  }
+  let answers= []; 
+  submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) { answers.push(answer);
+            console.log(answers);}
+  
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+            
+            
+        }
+        
+        currentQuiz++;
+        
+  
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            
+            
+  /* 
+  */
+           let results= quizData.map ((question,i) => 
+           
+           
+           ` 
+                <div class="${quizData[i].correct===answers[i]?
+                    "correct": "false" } quiz-header ">
+                <h2 id="question">${question.question}</h2>
+                <ul id="resultquestion"  >
+                  <li >
+                    <input type="radio" name="answer" id="a" class="answer">
+                    <label for="a" id="a_text">${question.a}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="b" class="answer">
+                    <label for="b" id="b_text">${question.b}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="c" class="answer">
+                    <label for="c" id="c_text">${question.c}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="d" class="answer">
+                    <label for="d" id="d_text">${question.d}</label>
+                  </li>
+                  <li>
+                  <h4>Správná odpověď: ${question[quizData[i].correct]}</h4>
+                <h4>Vybral jsi: ${question[answers[i]]}</h4>
+  
+                
+                  </li>
+                  
+                </ul>
+              </div>`
+                
+                )
+  
+  //                 result.classList.add("correct")
+  // /* 
+                
+  
+                /* quizData.forEach ((question,i) =>
+                 quizData[i].correct===answers[i]?
+                  result.classList.add("correct")  : result.classList.add("false") 
+                 ) */
+            
+         
+  
+  
+            quiz.innerHTML = `
+                <h2>Získal jsi ${score} bodů z ${quizData.length}.</h2>
+               
+  
+                
+                
+               
+    
+                ${results}
+  
+  
+                
+  
+                <button onclick="location.reload()">Znovu zkusit</button>
+            `
+        
+        
+        }
+    }
+  })
